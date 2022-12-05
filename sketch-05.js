11f971108034f3c2bde6d337b48d0b2a2a7c09bd
Melin Kan <melin.kan@outlook.com>
2022-12-05 21:29:36 +0300

    Create sketch-05

---
+++ sketch-05.js
@@ -0,0 +1,48 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [1080, 1080]
+};
+
+let text = 'M';
+let fontSize = 500;
+let fontFamily = 'serif';
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+    context.font = `${fontSize}px ${fontFamily}`;
+    context.textBaseline = 'middle';
+
+    const metrics = context.measureText(text);
+    const mx = metrics.actualBoundingBoxLeft * -1;
+    const my = metrics.actualBoundingBoxAscent * -1;
+    const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+    const x = (width - mw) * 0.5 - mx;
+    const y = (height - mh) * 0.5 - my;
+
+    context.save();
+    context.translate(x, y);
+
+    context.beginPath();
+    context.rect(mx, my, mw, mh);
+    context.stroke();
+
+    context.fillText(text, 0, 0);
+    context.restore();
+  };
+};
+
+const onKeyUp = (e) => {
+  text = e.key;
+  console.log(e);
+};
+
+document.addEventListener('keyup', onKeyUp);
+
+canvasSketch(sketch, settings);
